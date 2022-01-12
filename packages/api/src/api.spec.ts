acde4d20a9bec3da5920f23c22764534508ec264
Jake Hartnell <jake.hartnell@gmail.com>
2022-01-11 18:27:03 -0800

    v0.4.1

--- packages/api/src/api.spec.ts
+++ packages/api/src/api.spec.ts
@@ -1,15 +1,14 @@
-import { StargazeApi } from './api';
-import { QueryClientImpl } from './generated/cosmos/bank/v1beta1/query';
-import { DirectSecp256k1HdWallet, Registry } from '@cosmjs/proto-signing';
+import { StargazeApi } from "./api";
+import { QueryClientImpl } from "./generated/cosmos/bank/v1beta1/query";
+import { DirectSecp256k1HdWallet, Registry } from "@cosmjs/proto-signing";
 import {
-	SigningStargateClient,
-	assertIsBroadcastTxSuccess,
-} from '@cosmjs/stargate';
-import { MsgPost } from './generated/stargaze/curating/v1beta1/tx';
-import { coins } from '@cosmjs/launchpad';
+  SigningStargateClient,
+  assertIsBroadcastTxSuccess,
+} from "@cosmjs/stargate";
+import { coins } from "@cosmjs/launchpad";
 
 let api: StargazeApi;
-const chainUrl = 'localhost:26657';
+const chainUrl = "localhost:26657";
 
 /***
 
@@ -20,73 +19,73 @@ Fund with:
 
 */
 
-describe('StargazeApi', () => {
-	beforeAll(async () => {
-		api = await StargazeApi.connect({
-			connection: {
-				type: 'tendermint',
-				url: chainUrl,
-			},
-		});
-	});
+describe("StargazeApi", () => {
+  beforeAll(async () => {
+    api = await StargazeApi.connect({
+      connection: {
+        type: "tendermint",
+        url: chainUrl,
+      },
+    });
+  });
 
-	describe('Queries', () => {
-		it('should fetch balances using tendermint client', async () => {
-			const impl = new QueryClientImpl(api.connection.queryConnection);
-			const res = await impl.AllBalances({
-				address: 'stars1wsrvdmgfs0gugen4t4ak7hnudhy9mgnpcys5gn', // Shane's account
-			});
+  describe("Queries", () => {
+    it("should fetch balances using tendermint client", async () => {
+      const impl = new QueryClientImpl(api.connection.queryConnection);
+      const res = await impl.AllBalances({
+        address: "stars1wsrvdmgfs0gugen4t4ak7hnudhy9mgnpcys5gn", // Shane's account
+      });
 
-			expect(res.balances).toHaveLength(4);
-		});
-	});
+      expect(res.balances).toHaveLength(4);
+    });
+  });
 
-	describe('Txs', () => {
-		it('should post a tx on chain', async () => {
-			const mnemonic =
-				'surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put';
-			const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
-				mnemonic,
-				{ prefix: 'stars' }
-			);
-			const [{ address }] = await wallet.getAccounts();
+  // describe('Txs', () => {
+  // 	it('should post a tx on chain', async () => {
+  // 		const mnemonic =
+  // 			'surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put';
+  // 		const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+  // 			mnemonic,
+  // 			{ prefix: 'stars' }
+  // 		);
+  // 		const [{ address }] = await wallet.getAccounts();
 
-			const msgPostTypeUrl = '/stargaze.curating.v1beta1.MsgPost';
-			const registry = new Registry();
-			registry.register(msgPostTypeUrl, MsgPost);
+  // 		const msgPostTypeUrl = '/stargaze.curating.v1beta1.MsgPost';
+  // 		const registry = new Registry();
+  // 		registry.register(msgPostTypeUrl, MsgPost);
 
-			const client = await SigningStargateClient.connectWithSigner(
-				chainUrl,
-				wallet,
-				{ registry: registry }
-			);
+  // 		const client = await SigningStargateClient.connectWithSigner(
+  // 			chainUrl,
+  // 			wallet,
+  // 			{ registry: registry }
+  // 		);
 
-			const msg = MsgPost.fromPartial({
-				vendorId: 1,
-				postId: '1',
-				creator: address,
-				rewardAccount: address,
-				body: 'This is a tweet',
-			});
+  // 		const msg = MsgPost.fromPartial({
+  // 			vendorId: 1,
+  // 			postId: '1',
+  // 			creator: address,
+  // 			rewardAccount: address,
+  // 			body: 'This is a tweet',
+  // 		});
 
-			const msgAny = {
-				typeUrl: msgPostTypeUrl,
-				value: msg,
-			};
+  // 		const msgAny = {
+  // 			typeUrl: msgPostTypeUrl,
+  // 			value: msg,
+  // 		};
 
-			const fee = {
-				amount: coins(10000000, 'ustarx'),
-				gas: '180000', // 180k
-			};
-			const memo = 'Use your power wisely';
-			const result = await client.signAndBroadcast(
-				address,
-				[msgAny],
-				fee,
-				memo
-			);
+  // 		const fee = {
+  // 			amount: coins(10000000, 'ustarx'),
+  // 			gas: '180000', // 180k
+  // 		};
+  // 		const memo = 'Use your power wisely';
+  // 		const result = await client.signAndBroadcast(
+  // 			address,
+  // 			[msgAny],
+  // 			fee,
+  // 			memo
+  // 		);
 
-			assertIsBroadcastTxSuccess(result);
-		});
-	});
+  // 		assertIsBroadcastTxSuccess(result);
+  // 	});
+  // });
 });
